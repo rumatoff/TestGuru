ceadfa63fa13395b96255255e1e0e7a470c0ec83
Anton Antonov <rumatoff@gmail.com>
2019-08-23 20:36:13 +0300

    fix tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins("INNER JOIN results ON results.test_id = tests.id INNER JOIN users ON results.user_id = users.id WHERE users.id = #{id} AND tests.level = #{level}").pluck(:title)
+    join = 'INNER JOIN results ON results.test_id = tests.id INNER JOIN users ON
+          results.user_id = users.id'
+    Test.joins(join).where(results: { user_id: id }, tests: { level: level })
   end
 end
