bc1b6b7e4003e7066cf51e40f7ab7037e54200ed
Anton Antonov <rumatoff@gmail.com>
2019-08-24 22:00:15 +0300

    add associations, refactor scope, add new assoc. for test author

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,13 @@
 class Test < ApplicationRecord
-  def self.by_category(title)
-    join = 'INNER JOIN categories ON categories.id = category_id'
-    joins(join).where(categories: { title: title }).order(id: :desc).pluck(:title)
-  end
+
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+  belongs_to :category
+  has_one :author, foreign_key: 'id', class_name: 'User'
+
+  scope :by_category, lambda { |title|
+    joins(:category).where(categories: { title: title })
+                    .order(id: :desc).pluck(:title)
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_125423) do
+ActiveRecord::Schema.define(version: 2019_08_24_183417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_08_23_125423) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
