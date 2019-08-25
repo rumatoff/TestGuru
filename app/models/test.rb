76d32e1f96b287f8c219f5a3baa3f01c3c052b13
Anton Antonov <rumatoff@gmail.com>
2019-08-25 13:21:20 +0300

    refactor tests_by_level for User

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,16 @@
 class Test < ApplicationRecord
 
   belongs_to :category
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :users, through: :results
 
   scope :by_category, lambda { |title|
     joins(:category).where(categories: { title: title })
-                    .order(id: :desc).pluck(:title)
+                    .order(id: :desc)
   }
+  def self.pretty_by_category(title)
+    by_category(title).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
 
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
-  has_many :my_test, foreign_key: 'author_id', class_name: 'Test'
+  has_many :my_tests, foreign_key: 'author_id', class_name: 'Test'
 
   def tests_by_level(level)
-    Test.joins(:results).where(results: { user_id: id }, tests: { level: level })
+    my_tests.where(tests: { level: level })
   end
 end
