8141392855c97b03bd282288273e3d99663b1ff7
Anton Antonov <rumatoff@gmail.com>
2019-08-25 17:53:33 +0300

    fix tests_by_level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :my_tests, foreign_key: 'author_id', class_name: 'Test'
 
   def tests_by_level(level)
-    my_tests.where(tests: { level: level })
+    tests.where(tests: { level: level })
   end
 end
