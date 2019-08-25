9a1d4c6fc5352f194fb1636511ebfdf39dd90fa6
Anton Antonov <rumatoff@gmail.com>
2019-08-25 12:57:22 +0300

    refactor belongs_to places

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,10 @@
 class Test < ApplicationRecord
 
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :users, through: :results
-  belongs_to :category
-  belongs_to :author, class_name: 'User'
 
   scope :by_category, lambda { |title|
     joins(:category).where(categories: { title: title })
