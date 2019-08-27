dd8126900b8ca0587255649f64fdca6761ba22fb
Anton Antonov <rumatoff@gmail.com>
2019-08-27 20:00:22 +0300

    fix answers count validation for qestion

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,9 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
+  validate :validate_answers_count
+
+  def validate_answers_count
+    errors.add(:base, 'количество от 1 до 4') if question.answers.count > 4
+  end
 end
