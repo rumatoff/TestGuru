28998f578fe3006ab7b2b103398a851b42d88dad
Anton Antonov <rumatoff@gmail.com>
2019-08-27 20:03:53 +0300

    fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,6 +8,6 @@ class Answer < ApplicationRecord
   validate :validate_answers_count
 
   def validate_answers_count
-    errors.add(:base, 'количество от 1 до 4') if question.answers.count > 4
+    errors.add(:base, 'количество от 1 до 4') if question.answers.count > 3
   end
 end
