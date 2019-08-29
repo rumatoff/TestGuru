671a53b3e9e7b807a2221c815e430c6e7940f68f
Anton Antonov <rumatoff@gmail.com>
2019-08-29 16:52:17 +0300

    add index and show for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,10 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all.where(test_id: params[:test_id])
+    # @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
 end
