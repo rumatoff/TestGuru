82f40112fef8f0757ea34dd2acc47d2ca88e4f68
Anton Antonov <rumatoff@gmail.com>
2019-08-30 08:51:34 +0300

    add header and footer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,13 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all.where(test_id: params[:test_id])
-    # @questions = Question.all
   end
 
   def show
     @question = Question.find(params[:id])
+  end
+
+  def new
+
   end
 end
