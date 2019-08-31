8a04188854ca7284329b7c30cd532985221cec01
Anton Antonov <rumatoff@gmail.com>
2019-08-31 12:25:55 +0300

    add find_question callback

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
 
   before_action :find_test
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -9,8 +10,6 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
-
     respond_to do |format|
       format.html
       format.text { render plain: @question.body }
@@ -31,7 +30,6 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
 
     redirect_to test_questions_path(params[:test_id])
@@ -45,6 +43,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id]) if params[:test_id]
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
