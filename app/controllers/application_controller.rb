162b911c56e20952a50d46074f924ead917403df
Anton Antonov <rumatoff@gmail.com>
2019-09-28 17:28:27 +0300

    add flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user!
-    redirect_to login_path unless current_user
+    redirect_to login_path, alert: 'Are you a Guru? Verify your email and password please' unless current_user
   end
 
   def current_user
