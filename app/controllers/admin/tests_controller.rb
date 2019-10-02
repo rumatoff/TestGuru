1155ee6adf6451a1aa7bf1e957abe5f29c1d7e8b
Anton Antonov <rumatoff@gmail.com>
2019-10-02 19:57:10 +0300

    add questions and answers to admin (controllers)

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,56 @@
+class Admin::TestsController < ApplicationController
+
+  before_action :set_test, only: %i[show edit update destroy start]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def create
+    @test = User.first.my_tests.new(test_params)
+
+    if @test.save
+      redirect_to admin_tests_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @test.destroy
+
+    redirect_to admin_tests_path
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :category_id)
+  end
+
+  def rescue_with_test_not_found
+    render plain: 'Test not found'
+  end
+end
