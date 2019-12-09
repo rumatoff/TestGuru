6d0c0e384b6cc36526f691d4064f13e2d5e1579c
Anton Antonov <rumatoff@gmail.co>
2019-12-09 12:03:27 +0300

    add test level to new test form

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -58,7 +58,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :category_id)
+    params.require(:test).permit(:title, :level, :category_id)
   end
 
   def rescue_with_test_not_found
