b51095131b858d4080fe1b82c7c20bf41213d9a4
Anton Antonov <rumatoff@gmail.com>
2020-04-13 12:59:04 +0300

    add badge show

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,15 @@
 class BadgesController < ApplicationController
+  before_action :set_badge, only: :show
 
   def index
     @badges = Badge.all
+  end
+
+  def show; end
+
+  private
+
+  def set_badge
+    @badge = Badge.find(params[:id])
   end
 end
